f4f07c801c3eb6ec36f35a1bdd1a2f420374d987
lislis <mail@lislis.de>
2018-11-23 17:08:12 +0100

    collatz example

--- rust/examples/exmpl4-hof.rs
+++ rust/examples/exmpl4-hof.rs
@@ -7,7 +7,6 @@
 // In essence, we get a chain of numbers.
 // It is thought that for all starting numbers, the chains finish at the number 1.
 
-
 fn odd(n: u32) -> bool {
     n % 2 == 1
 }
@@ -16,11 +15,12 @@ fn even(n: u32) -> bool {
     !odd(n)
 }
 
+#[allow(dead_code)]
 fn collatz_chain(num: u32) -> Vec<u32> {
     match num {
         1 => { vec!(1) },
         n if (even(n)) => {
-            let v: Vec<u32> = vec!(n);
+            let v = vec!(n);
             [&v[..], &collatz_chain(n/2)[..]].concat()
         },
         n => {
@@ -30,13 +30,34 @@ fn collatz_chain(num: u32) -> Vec<u32> {
     }
 }
 
+fn collatz(num: u32) -> Vec<u32> {
+    let mut acc: Vec<u32> = vec!(num);
+    let mut n: u32 = num;
+
+    while n != 1 {
+        if even(n) {
+            n = n / 2;
+            acc.push(n);
+        } else {
+            n = (n * 3) +1;
+            acc.push(n)
+        }
+    }
+    acc
+}
+
 // for all starting numbers between 1 and 100, how many chains have a length greater than 15?
 
 fn main() {
-    //println!("{:?}", collatz_chain(10));
-    let chain_gt_15: usize =
-    (0..10).map(|n| collatz_chain(n))
-        .filter(|chain| &chain[..].len() > &15)
-        .count();
-    println!("{}", chain_gt_15);
+    println!("{:?}", collatz(30)); // loop
+    println!("{:?}", collatz_chain(30)); // recursion
+
+    // NOTE: this does not seem to properly work even tho it's compiling
+    // Run at your own risk
+
+    //let chain_gt_15: usize =
+    //    (0..10).map(|n| collatz(n))
+    //           .filter(|chain| chain.into_iter().count() > 15)
+    //           .count();
+    //println!("{}", chain_gt_15);
 }
